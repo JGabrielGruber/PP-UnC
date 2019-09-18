d546b026c6437bb28fc24ec798691ddfa6c41f43
José Gabriel Gruber <jgabrielgruber@hotmail.com>
2019-09-18 14:55:12 -0300

    Aperfeiçoada a credibilidade;

--- src/component/Copyright.component.js
+++ src/component/Copyright.component.js
@@ -1,16 +1,25 @@
 import React from 'react';
 import { Typography, Link } from "@material-ui/core";
+import FavoriteIcon from '@material-ui/icons/Favorite'
 
 function Copyright() {
 	return (
-		<Typography variant="body2" color="textSecondary" align="center">
-			{'Copyright © '}
-			<Link color="inherit" href="https://unc.br/">
-				UnC
-			</Link>{' '}
-			{new Date().getFullYear()}
-			{'.'}
-		</Typography>
+		<div>
+			<Typography variant="body2" color="textSecondary" align="center">
+				{'Copyright © '}
+				<Link color="inherit" href="https://www.unc.br/">
+					UnC
+				</Link>{' '}
+				{new Date().getFullYear()}
+				{'.'}
+			</Typography>
+			<Typography variant="body2" color="textSecondary" align="center">
+				{'Made with '} <FavoriteIcon style={{fontSize: "0.875rem"}} /> {' by '}
+				<Link color="inherit" href="https://www.github.com/JGabrielGruber">
+					J. Gabriel Gruber
+				</Link>
+			</Typography>
+		</div>
 	)
 }
 
