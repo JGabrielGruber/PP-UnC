2dff951b4dd6a1550127ebcf48b850584e40adf3
José Gabriel Gruber <jgabrielgruber@hotmail.com>
2019-10-02 19:43:00 -0300

    Implementado constantes contendo URL;

---
+++ src/conf.js
@@ -0,0 +1,4 @@
+const url_web	= "http://localhost:3000"
+const url_api	= "http://localhost:8000"
+
+export { url_api, url_web }
--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import { render } from 'react-dom'
+import axios from 'axios'
 
 import * as serviceWorker from './serviceWorker';
 import configureStore from './configureStore'
 import Root from './component/Root.component'
+import { url_api } from './conf'
 
 const store = configureStore()
+axios.defaults.baseURL = url_api
 
 render(
 	<Root store={store} />,
